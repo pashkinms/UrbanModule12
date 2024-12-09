edcad27fd6256d164da2cd0a898f1cd2e033e731
AlcheMystic <pashkinms86@gmail.com>
2024-12-10 04:40:34 +0700

    Added Task4

---
+++ Practice4_RTlogging.py
@@ -0,0 +1,96 @@
+import unittest
+import logging
+from rt_with_exceptions import Runner, Tournament
+
+
+class RunnerTest(unittest.TestCase):
+    is_frosen = False
+    
+    @unittest.skipIf(is_frosen, "Tests are frozen in this case.")
+    def type_test(self):
+        try:
+            name, speed = 2, 3
+            human = Runner(name , speed)
+        except TypeError as exp:
+            logging.error(f"Неверный тип имени {name}: {exp}")
+        try:
+            name, speed = 'Ванечка', -10
+            human = Runner(name , speed)
+        except ValueError as exp:
+            logging.error(f"Ошибка в значении скорости {speed}: {exp}")
+            
+    @unittest.skipIf(is_frosen, "Tests are frozen in this case.")
+    def test_walk(self):
+        human = Runner('Vasya', 5)
+        for i in range(10):
+            human.walk()
+        self.assertEqual(human.distance, 50)
+    
+    @unittest.skipIf(is_frosen, "Tests are frozen in this case.")
+    def test_run(self):
+        human = Runner('Vasya',5)
+        for i in range(10):
+            human.run()
+        self.assertEqual(human.distance, 100)
+   
+    @unittest.skipIf(is_frosen, "Tests are frozen in this case.")
+    def test_challenge(self):
+        human1 = Runner('Vasilisa',5)
+        human2 = Runner('Vasya', 10)
+        for i in range(10):
+            human1.walk()
+            human2.run()
+        self.assertNotEqual(human1.distance, human2.distance)
+        
+class TournamentTest(unittest.TestCase):
+    is_frosen = False
+    
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.all_results={}
+        return super().setUpClass()
+        
+    @unittest.skipIf(is_frosen, 'Tests are frozen in this case!')
+    def setUp(self) -> None:
+        self.runner1 = Runner('Usane', 10)
+        self.runner2 = Runner('Andrew', 9)
+        self.runner3 = Runner('Nick', 3)
+        return super().setUp()
+    
+    @classmethod
+    def tearDownClass(cls) -> None:
+        return super().tearDownClass()
+    
+    @unittest.skipIf(is_frosen, 'Tests are frozen in this case!')
+    def test12(self):
+        self.tournament = Tournament(90, self.runner1, self.runner3)
+        self.all_results = self.tournament.start()
+        self.assertTrue(self.all_results[2] == self.runner3)    
+    
+    @unittest.skipIf(is_frosen, 'Tests are frozen in this case!')
+    def test13(self):    
+        self.tournament = Tournament(90, self.runner2, self.runner3)
+        self.all_results = self.tournament.start()
+        self.assertTrue(self.all_results[2] == self.runner3)
+    
+    @unittest.skipIf(is_frosen, 'Tests are frozen in this case!')    
+    def test123(self):
+        self.tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
+        self.all_results = self.tournament.start()
+        self.assertTrue(self.all_results[3] == self.runner3)
+    
+    @unittest.skipIf(True, 'Tests are frozen in this case!')    
+    def test_overun(self):
+        self.tournament = Tournament(8, self.runner3, self.runner2, self.runner1)
+        self.all_results = self.tournament.start()
+        print(self.all_results)
+        self.assertTrue(self.all_results[3] == self.runner3) 
+        self.assertTrue(self.all_results[2] == self.runner2)
+        self.assertTrue(self.all_results[1] == self.runner1)   
+        
+        
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='utf-8',
+                        format='%(asctime)s | %(levelname)s | %(message)s')  
+    unittest.main(verbosity=2)
+    
--- rt_with_exceptions.py
+++ rt_with_exceptions.py
@@ -1,8 +1,14 @@
 class Runner:
     def __init__(self, name, speed=5):
-        self.name = name
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
         self.distance = 0
-        self.speed = speed
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
 
     def run(self):
         self.distance += self.speed * 2
@@ -11,6 +17,9 @@ class Runner:
         self.distance += self.speed
 
     def __str__(self):
+        return self.name
+
+    def __repr__(self):
         return self.name
 
     def __eq__(self, other):
@@ -33,12 +42,14 @@ class Tournament:
                 participant.run()
                 if participant.distance >= self.full_distance:
                     finishers[place] = participant
-                    if place > 1:
-                        for i in range(1, place):
-                            if finishers[place].distanse > finishers[i].distanse:
-                                finishers[place], finishers[i] = finishers[i], finishers[place]
                     place += 1
                     self.participants.remove(participant)
 
-                    
         return finishers
+
+#first = Runner('Вося', 10)
+#second = Runner('Илья', 5)
+#third = Runner('Арсен', 10)
+
+#t = Tournament(101, first, second)
+#print(t.start())
